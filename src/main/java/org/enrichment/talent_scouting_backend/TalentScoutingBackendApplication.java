1e7bf08d9d3281a147592cae911cac0ca29f5c4b
Josua Golden <josuagolden14@gmail.com>
2024-05-15 10:04:04 +0700

    Init

---
+++ src/main/java/org/enrichment/talent_scouting_backend/TalentScoutingBackendApplication.java
@@ -0,0 +1,13 @@
+package org.enrichment.talent_scouting_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentScoutingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalentScoutingBackendApplication.class, args);
+    }
+
+}
